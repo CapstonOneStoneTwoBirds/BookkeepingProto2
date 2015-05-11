ba2edff6dfefab612d4a9667243a06479e63347e
yog080787 <yog080787@gmail.com>
2015-05-11 23:21:16 +0900

    commit

--- app/src/main/java/com/capstone/bookkeepingproto2/PrivateHouseKeeping/Group/GroupActivity.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.bookkeepingproto2.PrivateHouseKeeping.Group;
-
-/**
- * Created by New on 2015-05-11.
- */
-public class GroupActivity {
-}
---
+++ app/src/main/java/com/capstone/bookkeepingproto2/PrivateHouseKeeping/GroupHouseKeeping/GroupActivity.java
@@ -0,0 +1,7 @@
+package com.capstone.bookkeepingproto2.PrivateHouseKeeping.GroupHouseKeeping;
+
+/**
+ * Created by New on 2015-05-11.
+ */
+public class GroupActivity {
+}
