968eea91184922ab17ecf892d0977f7ee00f7a0e
StigmaKim <jjang9c@naver.com>
2015-05-12 00:38:53 +0900

    something changed.

--- app/src/main/java/com/capstone/bookkeepingproto2/GroupHouseKeeping/GroupActivity.java
+++ app/src/main/java/com/capstone/bookkeepingproto2/GroupHouseKeeping/GroupActivity.java
@@ -1,7 +1,20 @@
 package com.capstone.bookkeepingproto2.GroupHouseKeeping;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.capstone.bookkeepingproto2.R;
+
 /**
  * Created by New on 2015-05-11.
  */
-public class GroupActivity {
+public class GroupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.group_main);
+    }
+
+    
 }
