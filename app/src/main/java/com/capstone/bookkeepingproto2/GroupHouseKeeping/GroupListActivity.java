71e212bcdac55dbecb67b4bebe3412deb4954b4c
StigmaKim <jjang9c@naver.com>
2015-05-14 14:56:53 +0900

    dfsdf
    sdfsdf

--- app/src/main/java/com/capstone/bookkeepingproto2/GroupHouseKeeping/GroupListActivity.java
+++ app/src/main/java/com/capstone/bookkeepingproto2/GroupHouseKeeping/GroupListActivity.java
@@ -18,6 +18,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 /**
  * YoungHoonKim
  * Created by New on 2015-05-11.
@@ -46,38 +48,39 @@ public class GroupListActivity extends Activity implements View.OnClickListener 
                         JSONArray jsonarr = new JSONArray(new String(responseBody));
                         System.out.println("json result : " + jsonarr);
 
-                        final TextView tv = (TextView)findViewById(R.id.group1_tv);
+                        final TextView tv1 = (TextView)findViewById(R.id.group1_tv);
+                        final TextView tv2 = (TextView)findViewById(R.id.group2_tv);
+                        final TextView tv3 = (TextView)findViewById(R.id.group3_tv);
+                        final TextView tv4 = (TextView)findViewById(R.id.group4_tv);
+
+                        ArrayList<TextView> tvlist = new ArrayList();
+                        tvlist.add(tv1);
+                        tvlist.add(tv2);
+                        tvlist.add(tv3);
+                        tvlist.add(tv4);
+
 
                         if( jsonarr.length() != 0 ){
                             //System.out.println(jsonarr.get(0));
-                            //final JSONObject got = new JSONObject(jsonarr.get(0).toString());
+                            //JSONObject got= new JSONObject(jsonarr.get(0).toString());
 
-                            for (int i=0;jsonarr.get(i)!=null;i++) {
+                            for (int i=0;i < jsonarr.length();i++) {
                                 System.out.println(jsonarr.get(i));
                                 final JSONObject got = new JSONObject(jsonarr.get(i).toString());
                                 System.out.println("json check : " + i);
+                                tvlist.get(i).setText(got.get("groupname").toString());
 
-                                tv.setText(got.get("groupname").toString());
+                                tvlist.get(i).setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(getApplicationContext(), GroupMainActivity.class);
+                                        try {
+                                            intent.putExtra("_id", got.get("_id").toString());
+                                        }catch( JSONException e){ }
+                                        startActivity(intent);
+                                    }
+                                });
                             }
-
-
-
-                            /*
-                            tv.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Intent intent = new Intent(getApplicationContext(), GroupMainActivity.class);
-                                    try {
-                                        intent.putExtra("_id", got.get("_id").toString());
-                                    }catch( JSONException e){ }
-                                    startActivity(intent);
-
-                                }
-                            });
-
-                            tv.setText(got.get("groupname").toString());
-                            */
-
                         }
                     } catch (JSONException e) { System.out.println(e); }
                 }
@@ -95,6 +98,7 @@ public class GroupListActivity extends Activity implements View.OnClickListener 
     public void onClick(View v) {
         if(v.getId() == R.id.newgroup_btn){
             Intent intent = new Intent(this, CreateGroupActivity.class);
+
             startActivity(intent);
 
         }
